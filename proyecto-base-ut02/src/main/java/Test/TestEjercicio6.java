9755097b2b535823fae08180d6761f3e33e09cfc
valentina <VALENTINA.MORALESB@correo.ucu.edu.uy>
2026-04-28 18:19:43 -0300

    Ejercicio6

---
+++ proyecto-base-ut02/src/main/java/Test/TestEjercicio6.java
@@ -0,0 +1,26 @@
+package Test;
+import junit.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import Impl.ABBImpl;
+
+public class TestEjercicio6 extends TestCase{
+    private ABBImpl<Integer> arbol;
+
+    public TestEjercicio6(String testName) { //constructor de la clase test
+        super(testName);
+    }
+
+    public static Test suite() { //crea un conjunto de tests para después ejecutar juntos
+        return (Test) new TestSuite(TestEjercicio6.class);
+    }
+
+    protected void setUp() {
+        arbol = new ABBImpl<>(); //se ejecuta antes de cada test, entorno
+    }    
+
+    public void altura(){
+        assertEquals(0,arbol.altura());
+    }
+}
